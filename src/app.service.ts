68a8c55e5aef76ee2a132d339a793a9ec9a4ebff
Marcelo Fernandez <marceloleandrofernandez@gmail.com>
2021-10-24 20:28:17 -0300

    useValue, useFactory, Global Modules

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('API_KEY') private apikey: string,
+    @Inject('TASKS') private tasks: any[],
+  ) {
+    console.log(apikey);
+  }
   getHello(): string {
-    return 'Hello World!';
+    console.log(this.tasks);
+    return `Hello World! ${this.apikey}`;
   }
 }
