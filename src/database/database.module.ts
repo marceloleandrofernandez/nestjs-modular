cc14b34223d8c2f7b24860112746b3eff6858412
Marcelo Fernandez <marceloleandrofernandez@gmail.com>
2021-10-25 08:28:16 -0300

    heroku Round 2

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -8,7 +8,7 @@ const API_KEY_PROD = 'PROD1234123412341234';
   providers: [
     {
       provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+      useValue: process.env.NODE_ENV === 'production' ? API_KEY_PROD : API_KEY,
     },
   ],
   exports: ['API_KEY'],
